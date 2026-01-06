c152d0c7c506ecad93fb1f64bd6955aa79e6d2ba
Jozef Galbička <galb.jozef@gmail.com>
2026-01-06 15:00:27 +0100

    update config for nvim-tree

--- lua/user/plugins/navigation.lua
+++ lua/user/plugins/navigation.lua
@@ -139,10 +139,15 @@ return {
         opts = {
             sync_root_with_cwd = true,
             renderer = { -- https://github.com/nvim-tree/nvim-tree.lua/discussions/1921?sort=old
+                --root_folder_label = ":t",
                 root_folder_label = function(path)
                     path = path:gsub(os.getenv("HOME"), "~", 1)
                     return path:gsub("([a-zA-Z])[a-z]+", "%1") .. path:gsub(".*[^a-zA-Z].?", "")
                 end,
+                indent_markers = {
+                    enable = true,
+                    inline_arrows = true
+                }
             },
             tab = {
                 sync = {
